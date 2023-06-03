ba4c5246571f61d4601e3fcbb25deeeb003058c4
Gorpetrosyann <gor.petrosyan6.y@tumo.org>
2023-06-03 14:56:34 +0400

    Final

--- Final/script.js
+++ Final/script.js
@@ -7,6 +7,8 @@ var Eate = [];
 var AllEatere = [];
 var matrix = [];
 var socket = io();
+var mm = 1;
+var nn = 1;
 
 function generate(a,b){
   for (let i = 0; i < a; i++) {
@@ -24,6 +26,8 @@ function frame1(){
   var button = document.querySelector("#myButton");
   button.addEventListener('click', ()=>{
      frameRate(2)
+     mm = 3;
+     nn =3;
      document.getElementById("myP").innerHTML = "Ներկայիս եղանակը ՝ Ձմեռ";   
   })
 }
@@ -31,7 +35,15 @@ function frame2(){
   var button = document.querySelector("#myButton2");
   button.addEventListener('click', ()=>{
      frameRate(10)
+     mm = 2;
+     nn = 2;
      document.getElementById("myP").innerHTML = "Ներկայիս եղանակը ՝ Ամառ";   
+  })
+}
+function refresh(){
+  var button = document.querySelector("#myButton3");
+  button.addEventListener('click', ()=>{
+     location.reload();   
   })
 }
 
@@ -60,11 +72,12 @@ function objectsCreate() {
 
 
 function setup() {
-  createCanvas(380,380)
-  background("blue");
+  createCanvas(375,375)
+  background("white");
   frameRate(1)
   frame1()
   frame2()
+  refresh()
   objectsCreate()
 
 }
@@ -91,12 +104,24 @@ function draw(){
     for(let y = 0;y < matrix.length;y++){
         for(let x = 0; x < matrix[y].length; x++){
           if(matrix[y][x] == 1){
-                fill("green");
+            if(mm == 1){   
+            fill("green");
+            }else if(mm == 2){
+              fill("orange")
+            }else{
+              fill("white")
+            }
             }else if (matrix[y][x] == 0){
                 fill ("gray")
            }
            else if (matrix[y][x] == 2){
+             if(nn == 1){ 
               fill ("yellow")
+            }else if(nn == 2){
+              fill (38, 70, 83)
+            }else{
+              fill ("aqua")
+            }
            }
             else if (matrix[y][x] == 3){
               fill ("blue")
@@ -105,7 +130,7 @@ function draw(){
             }else if(matrix[y][x] == 5){
               fill("red")
             }else if(matrix[y][x] == 6){
-              fill("black")
+              fill(96, 108, 56)
             }
             rect(x * side,y * side, side, side);
         }
